4e4d1b6d420ef574a281921f6005c3180951d655
kazuya.zama <zama@restarts.co.jp>
2022-10-04 22:07:08 +0900

    fivecommit

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -6,15 +6,15 @@ const BlogItem = ({blog}) => {
     return (
         <div>
         {/* <img loader={({src}) => src} src={`${blog.image.url}?fm=webp`} alt={blog.title} className="py-5" /> */}
-        <Image className="py-5" loader={({src}) => src} src={blog.image.url} alt={blog.title} width={800} height={500} layout="responsive" 
+        <Image className="py-5 rounded" loader={({src}) => src} src={blog.image.url} alt={blog.title} width={800} height={500} layout="responsive" 
            />
 
         <Moment format="YYYY/MM/DD">
         <p >{blog.publishedAt}</p>
         </Moment>
-        <h1 className="text-4xl py-3">{blog.title}</h1>
+        <h1 className="text-2xl md:text-3xl py-5 font-semibold">{blog.title}</h1>
         <h3>{blog.description}</h3>
-        <div className="pt-3 pb-7 flex justify-end">
+        <div className="pt-8 pb-7 md:flex justify-end">
                 {blog.tags.map((tag) => (
                     <div className="badge badge-accent ml-2" key={tag} >{tag}</div> 
                    
@@ -33,8 +33,7 @@ const BlogItem = ({blog}) => {
 
         
         
-      
-        <p>{blog.category && `${blog.category.name}`}</p>
+  
         </div>
     );
 }
