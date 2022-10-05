5673833844808b25069139182422648b76c3e37c
kazuya.zama <zama@restarts.co.jp>
2022-10-05 23:25:14 +0900

    eightcommit

---
+++ pages/[slug].js
@@ -0,0 +1,90 @@
+import { client } from "../libs/client";
+import BlogItem from "../components/BlogItem";
+import SearchForm from "../components/SearchForm";
+
+export default function Article({ blog, draftKey }) {
+  return blog ? (
+    <>      
+      {/* プレビューモードであるという表示 */}
+      <main className="container mx-auto">
+      {draftKey && (
+        <div>
+          現在プレビューモードで閲覧中です。
+        </div>
+      )}
+      <contaier className="md:flex flex-row">
+
+        <div className="p-8 md:p-0 basis-3/4">
+          <BlogItem blog={blog}/>
+
+        </div>
+
+        <div className="basis-1/4">
+        
+          <SearchForm />
+        
+        
+        </div>
+
+      </contaier>   
+
+      </main>
+
+    </ >
+  ) : (
+    <div>no content</div>
+  )
+}
+
+
+export const getStaticPaths = async () => {
+  const data = await client.get({endpoint:"blog"});
+  const paths = data.contents.map((content) => `/${content.slug}`);
+  return { paths, fallback: true };
+};
+ 
+ 
+export const getStaticProps = async (context) => {
+   
+
+    const { params, previewData } = context
+    if (!params?.slug) {
+      throw new Error('Error: ID not found')
+    }
+  
+    const isDraft = (arg) => {
+      if (!arg?.draftKey) {
+        return false
+      }
+      return typeof arg.draftKey === 'string'
+    }
+  
+    const slug = params.slug;
+    /* requestのクエリパラメータを生成*/
+    const draftKey = isDraft(previewData)
+      ? { draftKey: previewData.draftKey }
+      : {}
+  
+    /* draftKeyを付与してリクエストを投げる */
+    try {
+      const data = await client.getListDetail({
+        // endpoint:"categories"
+        endpoint: "blog",
+        contentId: slug,
+        queries: draftKey
+      });
+      return {
+        props: {
+          blog: data,
+          ...draftKey,
+        
+        },
+      };
+    } catch (e) {
+      /* 失敗したら404 */
+      return { notFound: true }
+    }
+   
+     
+ 
+}
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -4,7 +4,7 @@ import CategoryIndex from "../../components/CategoryIndex";
 import BlogItem from "../../components/BlogItem";
 import SearchForm from "../../components/SearchForm";
 
-export default function BlogId({ blog,category }) {
+export default function BlogId({ blog,category}) {
     return (
     <>
         <Seo  
@@ -17,44 +17,111 @@ export default function BlogId({ blog,category }) {
 
         <div className="p-8 md:p-0 basis-3/4">
           <BlogItem blog={blog}/>
+    
         </div>
 
         <div className="basis-1/4">
           <CategoryIndex category={category}/>
           <SearchForm />
-         
+        
         
         </div>
 
       </contaier>   
- 
+
       </main>
     </>
     );
-  }
+}
 
-  // 静的生成のためのパスを指定します
-  export const getStaticPaths = async () => {
-    const data = await client.get({endpoint:"blog"});
-    const paths = data.contents.map((content) => `/blog/${content.id}`);
-    return {paths,fallback:false}
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({endpoint:"blog"});
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return {paths,fallback:false}
+  
+}
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({endpoint:"blog",contentId:id});
+  
     
+// カテゴリーコンテンツの取得
+  const categoryData = await client.get({endpoint:"categories"})
+  return {
+      props:{
+          blog:data,
+          category:categoryData.contents,
+      }
   }
+}
 
-  // データをテンプレートに受け渡す部分の処理を記述します
-  export const getStaticProps = async (context) => {
-    const id = context.params.id;
-    const data = await client.get({endpoint:"blog",contentId:id});
+
+    // // 静的生成のためのパスを指定します
+    // export const getStaticPaths = async () => {
+    //   const data = await client.get({endpoint:"blog"});
+    //   const paths = data.contents.map((content) => `/blog/${content.id}`);
+    //  
+    //   return {paths,fallback:true}
+    // }
+  
+    // // データをテンプレートに受け渡す部分の処理を記述します
+    // export const getStaticProps = async (context) => {
+   
+
+      //  // カテゴリーコンテンツの取得
+      //  const categoryData = await client.get({endpoint:"categories"})
+      //  return {
+      //      props:{
+      //          blog:data,
+      //          category:categoryData.contents,
+      //      }
+      //  }
+
+    //   const { params, previewData } = context
+    //   if (!params?.id) {
+    //     throw new Error('Error: ID not found')
+    //   }
+    
+    //   const isDraft = (arg) => {
+    //     if (!arg?.draftKey) {
+    //       return false
+    //     }
+    //     return typeof arg.draftKey === 'string'
+    //   }
+    
+    //   const id = params.id;
+    //   /* requestのクエリパラメータを生成*/
+    //   const draftKey = isDraft(previewData)
+    //     ? { draftKey: previewData.draftKey }
+    //     : {}
+    
+    //   /* draftKeyを付与してリクエストを投げる */
+    //   try {
+    //     const data = await client.getListDetail({
+    //       // endpoint:"categories"
+    //       endpoint: "blog",
+    //       contentId: id,
+    //       queries: draftKey
+    //     });
+    //     return {
+    //       props: {
+    //         blog: data,
+    //         ...draftKey,
+          
+    //       },
+    //     };
+    //   } catch (e) {
+    //     /* 失敗したら404 */
+    //     return { notFound: true }
+    //   }
      
-  // カテゴリーコンテンツの取得
-    const categoryData = await client.get({endpoint:"categories"})
-    return {
-        props:{
-            blog:data,
-            category:categoryData.contents,
-        }
-    }
-  }
+       
+   
+    // }
+  
 
 
   
