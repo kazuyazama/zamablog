e09d30295b0bb6e3689348e35ad30895071dd4b0
kazuya.zama <zama@restarts.co.jp>
2022-11-04 00:27:09 +0900

    リッチエディタにショートコードを入れるように改修

--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -5,7 +5,7 @@ const BlogItem = ({blog,toc}) => {
 
     return (
       <>
-        <div>
+        {/* <div>
         <Image className="py-5 rounded" loader={({src}) => src} src={blog.image.url} alt={blog.title} width={800} height={500} layout="responsive" 
            />
 
@@ -39,12 +39,60 @@ const BlogItem = ({blog,toc}) => {
 
         <div 
         dangerouslySetInnerHTML={{
-          __html:`${blog.body}`,
+          __html: `${blog.body}`,
         }} 
         />
 
         
   
+        </div> */}
+
+        <div>
+        <Image className="py-5 rounded" loader={({src}) => src} src={blog.image.url} alt={blog.title} width={800} height={500} layout="responsive" 
+           />
+
+        <Moment format="YYYY/MM/DD">
+        <p >{blog.publishedAt}</p>
+        </Moment>
+        <h1 className="text-2xl md:text-3xl py-5 font-semibold">{blog.title}</h1>
+        <h3>{blog.description}</h3>
+        <div className="pt-8 pb-7 md:flex justify-end">
+                {blog.tags.map((tag) => (
+                    <div className="badge badge-accent ml-2" key={tag} >{tag}</div> 
+                   
+                ))}
+        </div>
+
+        {blog.toc_visible && (
+           <div className="py-5 text-center " >
+          <p className="pb-3 font-bold text-xl">目次</p>
+          <ul>
+            {toc.map(data => (
+              <li key={data.id} className="text-lg py-1 link link-hover text-primary-content">
+                <a href={`#${data.id}`}>
+                  {data.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+          </div>
+        )}
+
+
+
+        
+
+    
+        <div 
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+          
+        }} 
+        />
+      
+
+        
+
         </div>
         </>
     );
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -5,7 +5,102 @@ import CategoryIndex from "../../components/CategoryIndex";
 import BlogItem from "../../components/BlogItem";
 import SearchForm from "../../components/SearchForm";
 
-export default function BlogId({ blog,category}) {
+// export default function BlogId({ blog,category}) {
+
+
+//   //目次作成用
+//   const renderToc = (body) => {
+//       const $ = cheerio.load(body);
+//       const headings = $('h1, h2, h3').toArray();
+//       const toc = headings.map((data) => ({
+//         text: data.children[0].data ,
+//         id: data.attribs.id
+//       }));
+  
+//     return toc;
+//   };
+
+
+
+  
+//   const toc = renderToc(blog.body);
+//   // console.log(toc)
+  
+//     return (
+//     <>
+//         <Seo  
+//         pageTitle={blog.title}
+//         pageDescription={blog.description}
+//         pageImg={blog.image.url}
+//         />
+//       <main className="container mx-auto">
+//       <contaier className="md:flex flex-row">
+
+//         <div className="p-8 md:p-0 basis-3/4">
+//           <BlogItem blog={blog} toc={toc}/>
+    
+//         </div>
+
+//         <div className="basis-1/4">
+//           <CategoryIndex category={category}/>
+//           <SearchForm />
+        
+        
+//         </div>
+
+//       </contaier>   
+
+//       </main>
+//     </>
+//     );
+// }
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({endpoint:"blog"});
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return {paths,fallback:false}
+  
+}
+
+
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({endpoint:"blog",contentId:id});
+  // const data = await client.get({endpoint:"blog",contentId:params?.id ?? ''});
+
+  // カテゴリーコンテンツの取得
+  const categoryData = await client.get({endpoint:"categories"})
+  
+  return {
+    props:{
+        category:categoryData.contents,
+        // blog:data,
+        data:{       //このデータはこの記事参考に作った　https://zenn.dev/aiji42/articles/46ac448d95e847
+          ...data,
+          body:replaceBody(data),
+        } 
+      },
+      revalidate: 3600
+  }
+}
+
+// ショートコードをリッチエディタに埋め込む為の関数
+export const replaceBody = ({ body, shortCodes }) => {
+  console.log(shortCodes)
+  const shortCodesMap =
+    shortCodes?.reduce(
+      (res, { code, body }) => ({ ...res, [code]: body }),
+      {}
+    ) ?? {}
+  return body
+    .replace(/&lt;&lt;(.+?)&gt;&gt;/g, (...[, key]) => shortCodesMap[key])
+}
+
+
+export default function BlogId({category,data}) {
 
 
   //目次作成用
@@ -19,22 +114,25 @@ export default function BlogId({ blog,category}) {
   
     return toc;
   };
+
   
-  const toc = renderToc(blog.body);
-  console.log(toc)
+  const toc = renderToc(data.body);
+  // console.log(toc)
   
     return (
     <>
         <Seo  
-        pageTitle={blog.title}
-        pageDescription={blog.description}
-        pageImg={blog.image.url}
+        pageTitle={data.title}
+        pageDescription={data.description}
+        pageImg={data.image.url}
         />
       <main className="container mx-auto">
       <contaier className="md:flex flex-row">
 
         <div className="p-8 md:p-0 basis-3/4">
-          <BlogItem blog={blog} toc={toc}/>
+          <BlogItem blog={data} toc={toc} 
+          // toc={toc}
+          />
     
         </div>
 
@@ -52,30 +150,10 @@ export default function BlogId({ blog,category}) {
     );
 }
 
-// 静的生成のためのパスを指定します
-export const getStaticPaths = async () => {
-  const data = await client.get({endpoint:"blog"});
-  const paths = data.contents.map((content) => `/blog/${content.id}`);
-  return {paths,fallback:false}
-  
-}
 
-// データをテンプレートに受け渡す部分の処理を記述します
-export const getStaticProps = async (context) => {
-  const id = context.params.id;
-  const data = await client.get({endpoint:"blog",contentId:id});
 
-  // カテゴリーコンテンツの取得
-  const categoryData = await client.get({endpoint:"categories"})
-  
-  return {
-      props:{
-          blog:data,
-          category:categoryData.contents,
-        
-      }
-  }
-}
+
+
 
 
     // // 静的生成のためのパスを指定します
